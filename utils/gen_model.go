7dc7f552033e14f37d11f627bfd2e53877cce840
zhiming.sun <zhiming.sun@ifchange.com>
2019-11-22 18:03:09 +0800

    update

--- utils/gen_model.go
+++ utils/gen_model.go
@@ -24,6 +24,12 @@ type {{.StructName}} struct {                               {{range $fieldIndex,
 	{{.Name}}   {{.Type}}   {{.Tag}}    // {{.Comment}}     {{end}}
 }
 
+type {{private .StructName}} struct {
+	List  []{{.StructName}}
+	Count int
+	Error error
+}
+
 func New{{.StructName}}() *{{.StructName}} {
     return new({{.StructName}})
 }
@@ -36,45 +42,49 @@ func (table *{{.StructName}}) DB() *gorm.DB {
 	return orm.Gorm().Table(table.Table())
 }
 
+// insert one
 func (table *{{.StructName}}) Insert() error {
 	return table.DB().Create(table).Error
 }
 
+// select one by id
 func (table *{{.StructName}}) Select() error {
 	return table.DB().Where("id = ? AND is_deleted = 0", table.ID).First(table).Error
 }
 
+// update one by id, but do not update field defaults
 func (table *{{.StructName}}) Update() error {
 	return table.DB().Where("id = ? AND is_deleted = 0", table.ID).Updates(table).Error
 }
 
+// delete one by id
 func (table *{{.StructName}}) Delete() error {
 	return table.DB().Where("id = ? AND is_deleted = 0", table.ID).Update(map[string]int{"is_deleted": 1}).Error
 }
 
-func (table *{{.StructName}}) Save(value map[string]interface{}) error {
-	return table.DB().Where("id = ? AND is_deleted = 0", table.ID).Save(value).Error
+// select multiple data in a single table
+func (table *{{.StructName}}) Selects(query interface{}, args ...interface{}) *{{private .StructName}} {
+	tables := new({{private .StructName}})
+	tables.Error = table.DB().Count(&tables.Count).Where(query, args).Find(&tables.List).Error
+	return tables
 }
 
-func (*{{.StructName}}) Inserts(tables *[]{{.StructName}}) error {
+// insert multiple data in a single table
+func (table *{{.StructName}}) Inserts(tables *[]{{.StructName}}) error {
 	swg := sync.WaitGroup{}
 	swg.Add(len(*tables))
-
 	errorchannel := make(chan error, len(*tables))
-
-	tx := orm.Gorm().Begin()
-
+	tx := table.DB().Begin()
 	defer func() {
 		if err := recover(); err != nil {
 			tx.Rollback()
 		}
 	}()
-
 	for _, table := range *tables {
 		go func(x *{{.StructName}}) {
 			defer swg.Done()
 
-			if err := tx.Table(x.Table()).Create(x).Error; err != nil {
+			if err := tx.Create(x).Error; err != nil {
 				errorchannel {{html "<-"}} err
 				log.Errorf(fmt.Sprintf("Inserts %s error: %v", x.Table(), err))
 			}
@@ -82,8 +92,22 @@ func (*{{.StructName}}) Inserts(tables *[]{{.StructName}}) error {
 	}
 	swg.Wait()
 	close(errorchannel)
-
 	for err := range errorchannel {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit().Error
+}
+
+// updates multiple data in a single table. field defaults can be updated
+func (table *{{.StructName}}) Updates(datas map[string]interface{}, query interface{}, args ...interface{}) error {
+	tx := table.DB().Begin()
+	defer func() {
+		if err := recover(); err != nil {
+			tx.Rollback()
+		}
+	}()
+	if err := tx.Where(query, args).Updates(datas).Error; err != nil {
 		tx.Rollback()
 		return err
 	}
@@ -114,6 +138,10 @@ func (config *GlobalConfig) GenModel(Struct *Struct, wr io.Writer) {
 				}
 			}
 			return "gorm.DB"
+		},
+		"private": func(source string) string {
+			return strings.ToLower(source)
+
 		},
 		"html": func(source string) template.HTML {
 			return template.HTML(source)
